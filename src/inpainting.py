58781fe05bfd2b37d3f8950db2858990ca3afd9a
vishnu vardhan <kurra.vishnuv@gmail.com>
2025-01-06 17:37:50 +0530

    small changes

--- src/inpainting.py
+++ src/inpainting.py
@@ -2,7 +2,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 
-def perform_inpainting(pipeline, image, mask, prompt, strength=0.6,num_inference_steps=50):
+def perform_inpainting(pipeline, image, mask, prompt, strength=1,num_inference_steps=50,guidance_scale=7):
     new_size = (512, 512)
     image_resized = Image.fromarray(image).resize(new_size)
     mask_resized = Image.fromarray((mask * 255).astype(np.uint8)).resize(new_size)
@@ -13,6 +13,7 @@ def perform_inpainting(pipeline, image, mask, prompt, strength=0.6,num_inference
         mask_image=mask_resized, 
         strength=strength,
         num_inference_steps=num_inference_steps,
+        guidance_scale=guidance_scale,
         )
 
     return result.images[0]
